b8989573954a8e611f05fb89a0bbad63e629a605
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-12-01 09:16:44 +0100

    🧾 Add license to package info

--- info.rkt
+++ info.rkt
@@ -2,6 +2,7 @@
 (define pkg-desc "Solutions to Advent of Code 2022")
 (define pkg-authors '(scorphus))
 (define version "0.0")
+(define license '(BSD-3-Clause))
 (define build-deps
   '("advent-of-code" ; --
     "cover" ; --
